9027986b41d4f05aa23aff1c64635e13f2de5aa4
kopib <arkoroundpi@gmail.com>
2025-08-27 09:40:45 +0200

    add tests for column pre-selection

--- tests/test_core/test_loom_selected_columns.py
+++ tests/test_core/test_loom_selected_columns.py
@@ -6,7 +6,10 @@ weave and refine tasks, including the handling of special values like "all"
 and "none".
 """
 
-from weaveflow import Loom, weave
+import pytest
+from pandas import DataFrame
+
+from weaveflow import Loom, refine, weave
 
 
 @weave(outputs="z1")
@@ -27,6 +30,12 @@ def get_income_per_person(income_thousands: float, children: int, number_extra: 
 def get_satisfaction_score_per_income(satisfaction_score: float, income_per_person: float):
     """Calculate satisfaction score per income."""
     return satisfaction_score / income_per_person
+
+
+@refine
+def filter_data(df: DataFrame) -> DataFrame:
+    """Clean the data."""
+    return df[df["age"] > 30].dropna()
 
 
 def test_loom_selected_columns(base_dataframe_input):
@@ -64,3 +73,44 @@ def test_loom_columns_infer_weave_multiple_tasks(personal_data):
         "satisfaction_score",
         "satisfaction_score_per_income",
     ]
+
+
+def test_loom_columns_infer_refine(personal_data):
+    """Tests that Loom correctly infers columns from refine tasks."""
+    loom = Loom(
+        personal_data,
+        [filter_data, get_income_per_person, get_satisfaction_score_per_income],
+        infer_weave_columns=True,
+        refine_columns=["age"],  # take age as this is used for filtering data
+    )
+    # Assert weave columns are inferred correctly and refine columns are added
+    assert sorted(loom.database.columns.tolist()) == [
+        "age",
+        "children",
+        "income_thousands",
+        "satisfaction_score",
+    ]
+    loom.run()
+    assert all(loom.database["age"] > 30)
+    assert sorted(loom.database.columns.tolist()) == [
+        "age",
+        "children",
+        "income_per_person",
+        "income_thousands",
+        "satisfaction_score",
+        "satisfaction_score_per_income",
+    ]
+
+
+def test_loom_columns_unknown_column_error(personal_data):
+    """Tests that Loom correctly raises error on unknown column."""
+    with pytest.raises(
+        KeyError,
+        match="Required columns not found in DataFrame: \\['unknown_columns'\\]",
+    ):
+        Loom(
+            personal_data,
+            [filter_data, get_income_per_person, get_satisfaction_score_per_income],
+            infer_weave_columns=True,
+            refine_columns=["unknown_columns"],
+        )
