62f151e6d1498362e7d7b03cca4814d85662998b
kopib <arkoroundpi@gmail.com>
2025-08-27 10:16:02 +0200

    get rid of comment in init

--- weaveflow/core/__init__.py
+++ weaveflow/core/__init__.py
@@ -6,5 +6,4 @@ including the main orchestrator (Loom) and the graph visualization tools.
 from weaveflow.core.loom import Loom, PandasWeave
 from weaveflow.core.nxgraph import RefineGraph, WeaveGraph
 
-# --- Define main API for krystallizer module ---
 __all__ = ["Loom", "PandasWeave", "RefineGraph", "WeaveGraph"]
